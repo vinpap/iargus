1df6db6aef1593be549b56f0c76e288c4d946799
Vincent Papelard <vincent.papelard@logiroad-center.com>
2024-06-01 15:17:50 +0200

    Fixed a bug in setup_tests.py (no host was specified for MLflow)

--- setup_tests.py
+++ setup_tests.py
@@ -1,8 +1,10 @@
 """
 This script set ups everything needed to run the tests.
 """
+import os
 
 import pandas as pd
+import mlflow
 from mlflow import MlflowClient
 
 from model_monitoring import preprocess_features, train_model
@@ -13,6 +15,7 @@ X = preprocess_features(testing_data)
 y = testing_data["price"].values
 
 # If no model has been trained, we train one quickly.
+mlflow.set_tracking_uri(uri=os.environ["MLFLOW_HOST"])
 client = MlflowClient()
 model_versions = client.search_model_versions(f"name='iargus'")
 if len(model_versions) == 0:
