8f8a544385b6b60b64d77e25fe4ca962716fc477
Vincent Papelard <vincent.papelard@logiroad-center.com>
2024-06-01 15:04:54 +0200

    Edited fill_db.py to adjust to different column names in the data

--- fill_db.py
+++ fill_db.py
@@ -9,7 +9,7 @@ import mysql.connector
 import pandas as pd
 
 with mysql.connector.connect(
-        host="localhost",
+        host=os.environ["MYSQL_HOST"],
         user=os.environ["MYSQL_USER"],
         password=os.environ["MYSQL_PWD"],
         database=os.environ["MYSQL_DB_NAME"],
@@ -18,12 +18,13 @@ with mysql.connector.connect(
             current_date = date.today()
 
             df = pd.read_csv("./experiments/true_car_listings.csv").sample(n=300000)
-            df = df[["state", "make", "model", "year", "mileage", "price"]]
-            df["state"] = df["state"].str.strip()
-            df["make"] = df["make"].str.strip()
-            df["model"] = df["model"].str.strip()
+            df = df[["State", "Make", "Model", "Year", "Mileage", "Price"]]
+            df["State"] = df["State"].str.strip()
+            df["Make"] = df["Make"].str.strip()
+            df["Model"] = df["Model"].str.strip()
             df["current_date"] = current_date.strftime("%Y-%m-%d")
             rows = list(df.itertuples(index=False, name=None))
+            rows=rows[:50000]
             values = ', '.join(map(str, rows))
             query = f"""INSERT INTO car_details (state, make, model, year, mileage, price, date_added) VALUES {values}"""
             print(query[:500])
